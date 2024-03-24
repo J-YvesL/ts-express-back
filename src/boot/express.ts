0e306d57aa057d7af109a51e0d404a65f314be15
Jean-Yves Lalanne <jylalanne@demboost.com>
2024-03-24 12:05:25 +0100

    CONF api versionning config

--- src/boot/express.ts
+++ src/boot/express.ts
@@ -1,9 +1,9 @@
 import express, { Express } from 'express';
-import publicRouter from '@/api/public';
-import privateRouter from '@/api/private';
+import publicRouterV1 from '@/api/v1/public';
+import privateRouterV1 from '@/api/v1/private';
 
 export default function setupExpress(app: Express) {
   app.use(express.json());
-  app.use('/api', publicRouter);
-  app.use('/api/private', privateRouter);
+  app.use('/api/v1', publicRouterV1);
+  app.use('/api/v1/private', privateRouterV1);
 }
