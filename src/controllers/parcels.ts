112424ebc57dfb832f30bb1894a7a8ca22e188ee
Jean-Yves Lalanne <jylalanne@demboost.com>
2024-03-24 17:06:28 +0100

    DEV parcels building

--- src/controllers/parcels.ts
+++ src/controllers/parcels.ts
@@ -17,8 +17,8 @@ export class ParcelsController {
     try {
       for (const [id, order] of Object.entries(orders)) {
         // Split order items into parcels.
-        const parcel = this.processOrder(id, order);
-        parcels.push(...parcel);
+        const newParcels = this.processOrder(id, order);
+        parcels.push(...newParcels);
       }
       // Request tracking IDs
       const trackingIds = await CarrierService.createDeliveryRequest(
@@ -38,8 +38,71 @@ export class ParcelsController {
     }
   }
 
+  /**
+   * Put all items of an order into parcels for delivery.
+   * @param id Order ID
+   * @param order Order content
+   * @returns Parcels ready for delivery
+   */
   private static processOrder(id: string, order: OrderRecord): Parcel[] {
-    const parcel: Parcel = { orderId: id, items: [], weight: 0 };
-    return [parcel];
+    const itemsToPack = this.convertToPackingList(order);
+    const parcels: Parcel[] = [];
+    let heaviestItem: { id: string; weight: number } | undefined;
+    while ((heaviestItem = itemsToPack.shift()) !== undefined) {
+      let found = false;
+      // Try to find a parcel that can accept this weight
+      for (const parcel of parcels) {
+        if (parcel.weight + heaviestItem.weight <= 30) {
+          found = true;
+          const existingItem = parcel.items.find(
+            item => item.item_id === heaviestItem!.id
+          );
+          if (existingItem) {
+            existingItem.quantity += 1;
+          } else {
+            parcel.items.push({ item_id: heaviestItem.id, quantity: 1 });
+          }
+          parcel.weight += heaviestItem.weight;
+        }
+      }
+      // If no suitable parcel could be found, then create a new Parcel
+      if (!found) {
+        const parcel: Parcel = {
+          orderId: id,
+          items: [{ item_id: heaviestItem.id, quantity: 1 }],
+          weight: heaviestItem.weight,
+        };
+        parcels.push(parcel);
+      }
+    }
+    return parcels;
+  }
+
+  /**
+   * Create a list of items from an Order. The list is ordered by weight DESC.
+   * @param order An order to pack into parcels
+   * @returns The list of items to pack
+   */
+  private static convertToPackingList(
+    order: OrderRecord
+  ): { id: string; weight: number }[] {
+    const itemsToPack: { id: string; weight: number }[] = [];
+    for (const item of order.items) {
+      const weight = ItemsController.getItemWeight(item.item_id);
+      if (weight === undefined) {
+        throw new Error(
+          `Item ID ${item.item_id}:Undefined item weight. Cannot build parcel.`
+        );
+      }
+
+      for (let i = 0; i < item.quantity; i++) {
+        itemsToPack.push({
+          id: item.item_id,
+          weight,
+        });
+      }
+    }
+    itemsToPack.sort((a, b) => b.weight - a.weight);
+    return itemsToPack;
   }
 }
